042a0bb24a9609b51e2f55ae818fb00e069f0484
Alberto Juliana <ajulianaq@gmail.com>
2023-05-23 12:41:18 +0200

    parte 1

--- ejs/1/Ejercicio_1_Juliana_Quiros_Alberto.R
+++ ejs/1/Ejercicio_1_Juliana_Quiros_Alberto.R
@@ -1,1 +1,22 @@
+
+library(mice)
+library(naniar)
+
+## 1.1
 data <- read.table("C:\\GDrive1\\Uni\\Master\\datos_perdidos\\ejs\\1\\Ejercicio1.dat", header = T)
+md.pattern(data)
+
+## 1.2
+data_noMOT <- data[,-4]
+md.pattern(data_noMOT)
+
+
+## 1.3
+REND <- ifelse(is.na(data$REND), 0, 1)
+data$REND <- REND
+
+t.test(data$HESTUDIO ~ data$REND)
+
+## 1.5
+
+vis_miss(data)
